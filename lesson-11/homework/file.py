43fc8245590d607e1c3928e97a74caae7ee9e916
MukhsinjonKodirov <kadirovmukhsinjon@gmail.com>
2025-04-02 12:33:46 -0400

    Update file.py

--- lesson-11/homework/file.py
+++ lesson-11/homework/file.py
@@ -1,1 +1,55 @@
+# Task 1
+
+python -m venv myenv
+myenv\Scripts\activate
+pip install numpy requests
+
+# Task 2
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+
+def reverse_string(s):
+    return s[::-1]
+
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    return sum(1 for char in s if char in vowels)
+
+# Task 3
+import math
+
+def calculate_area(radius):
+    return math.pi * radius ** 2
+
+def calculate_circumference(radius):
+    return 2 * math.pi * radius
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return "File not found."
+
+
+def write_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+
+
 
